120e0e7e96c882f0e34183bdda2cf468609fcb37
Nicola Ibrahim <nicolaibrahim969@gmail.com>
2025-08-16 22:41:58 +0200

    Update the create method signature

--- src/modules/optimization_engine/infrastructure/metrics.py
+++ src/modules/optimization_engine/infrastructure/metrics.py
@@ -46,12 +46,26 @@ class MetricFactory:
     Concrete factory for creating various validation metric instances.
     """
 
-    def create(self, metric_type: str, **kwargs: Any) -> BaseValidationMetric:
+    def create(self, config: dict) -> BaseValidationMetric:
         """
         Creates and returns a concrete validation metric instance based on the given type and parameters.
         """
+        metric_type = config.get("type")
+        params = config.get("params", {})
+
         if metric_type == "MSE":
-            return MeanSquaredErrorValidationMetric(**kwargs)
-        # Add more metric types here as you implement them
+            return MeanSquaredErrorValidationMetric(**params)
+
+        # elif metric_type == "MAE":
+        #     return MeanAbsoluteErrorValidationMetric(**params)
+
+        # elif metric_type == "R2":
+        #     return R2ScoreValidationMetric(**params)
+
+        # elif metric_type == "MAPE":
+        #     return MeanAbsolutePercentageErrorValidationMetric(**params)
+
+        # elif metric_type == "SMAPE":
+        #     return SymmetricMeanAbsolutePercentageErrorValidationMetric(**params)
         else:
             raise ValueError(f"Unknown metric type: {metric_type}")
--- src/modules/optimization_engine/infrastructure/normalizers.py
+++ src/modules/optimization_engine/infrastructure/normalizers.py
@@ -254,20 +254,23 @@ class NormalizerFactory:
     Concrete factory for creating various normalizer instances.
     """
 
-    def create(self, normalizer_type: str, **kwargs: Any) -> BaseNormalizer:
+    def create(self, config: dict) -> BaseNormalizer:
         """
         Creates and returns a concrete normalizer instance based on the given type and parameters.
         """
+        normalizer_type = config.get("type")
+        params = config.get("params", {})
+
         if normalizer_type == "MinMaxScaler":
-            return MinMaxScalerNormalizer(**kwargs)
+            return MinMaxScalerNormalizer(**params)
         elif normalizer_type == "HypercubeNormalizer":
-            return HypercubeNormalizer(**kwargs)
+            return HypercubeNormalizer(**params)
         elif normalizer_type == "StandardNormalizer":
-            return StandardNormalizer(**kwargs)
+            return StandardNormalizer(**params)
         elif normalizer_type == "UnitVectorNormalizer":
-            return UnitVectorNormalizer(**kwargs)
+            return UnitVectorNormalizer(**params)
         elif normalizer_type == "LogNormalizer":
-            return LogNormalizer(**kwargs)
-        # Add more normalizer types here as you implement them
+            return LogNormalizer(**params)
+
         else:
             raise ValueError(f"Unknown normalizer type: {normalizer_type}")
